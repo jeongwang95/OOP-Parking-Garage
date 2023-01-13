775c02fa5276148d1ccb07add0e6902071a54f95
Tenzin Lama <tenchoelama80@gmail.com>
2023-01-13 11:07:31 -0600

    pay-for-parking

--- Garage.py
+++ Garage.py
@@ -13,7 +13,30 @@ class Garage():
             self.taken_spaces += 1
             print('Here is your ticket.')
 
-my_garage = Garage(10)
-my_garage.take_ticket()
-print(my_garage.tickets)
-print(my_garage.current_ticket)
+# my_garage = Garage(10)
+# my_garage.take_ticket()
+# print(my_garage.tickets)
+# print(my_garage.current_ticket)
+
+
+    def pay_for_parking(self):
+
+        ticket_no = int(input(" what is your ticket number? :"))
+
+        if ticket_no in self.current_ticket:
+            print(f"Your ticket number {ticket_no} costs $20.")
+            response = input("Would you like to pay for your ticket now? Enter y/n: ")
+
+            if response.lower().strip() in ('y','yes'):
+                self.current_ticket[ticket_no] = 0
+                print("Thank you for paying your ticket, please exit garage within 15 mins.")
+            else:
+                print("Okay! Please pay before you exit the garage.")
+            
+        else:
+            print(f"{ticket_no} is not valid. Please double check your {ticket_no}.")
+
+        
+    
+
+        
